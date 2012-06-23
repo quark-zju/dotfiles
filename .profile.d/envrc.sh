47be267455a6016660ec59578cbe35b27c754e67
WU Jun <quark@lihdd.net>
2012-06-23 10:27:29 +0800

    update envrc.sh:
    * check before setting locale
    * prepend not append $PATH

--- .profile.d/envrc.sh
+++ .profile.d/envrc.sh
@@ -1,27 +1,29 @@
 # locale
-export LANG=en_US.UTF-8
-export LC_CTYPE="zh_CN.UTF-8" # KDE needs this
-export LC_NUMERIC="en_US.UTF-8"
-export LC_TIME="en_US.UTF-8"
-export LC_COLLATE="en_US.UTF-8"
-export LC_MONETARY="en_US.UTF-8"
-export LC_MESSAGES="en_US.UTF-8"
-export LC_PAPER="en_US.UTF-8"
-export LC_NAME="en_US.UTF-8"
-export LC_ADDRESS="en_US.UTF-8"
-export LC_TELEPHONE="en_US.UTF-8"
-export LC_MEASUREMENT="en_US.UTF-8"
-export LC_IDENTIFICATION="en_US.UTF-8"
-export LESSCHARSET=utf-8
+if locale -a | grep -Fq 'zh_CN.utf8'; then
+    export LANG=en_US.UTF-8
+    export LC_CTYPE="zh_CN.UTF-8" # KDE needs this
+    export LC_NUMERIC="en_US.UTF-8"
+    export LC_TIME="en_US.UTF-8"
+    export LC_COLLATE="en_US.UTF-8"
+    export LC_MONETARY="en_US.UTF-8"
+    export LC_MESSAGES="en_US.UTF-8"
+    export LC_PAPER="en_US.UTF-8"
+    export LC_NAME="en_US.UTF-8"
+    export LC_ADDRESS="en_US.UTF-8"
+    export LC_TELEPHONE="en_US.UTF-8"
+    export LC_MEASUREMENT="en_US.UTF-8"
+    export LC_IDENTIFICATION="en_US.UTF-8"
+    export LESSCHARSET=utf-8
+fi
 
-# append PATHs
-local PATH_APPEND=''
+# prepend PATHs
+local PATH_PREPEND=''
 for p in /usr/lib/colorgcc/bin $HOME/batch .; do
-    if (! echo $PATH $PATH_APPEND | fgrep -q ":$p") && [ -e "$p" ]; then
-        PATH_APPEND=$PATH_APPEND:$p
+    if (! echo $PATH:$PATH_PREPEND | fgrep -q ":$p") && [ -e "$p" ]; then
+        PATH_PREPEND=${PATH_PREPEND}${p}:
     fi
 done
-export PATH=${PATH}${PATH_APPEND}
+export PATH=${PATH_PREPEND}${PATH}
 
 # wine: no shortcuts, no verbose output
 export WINEDLLOVERRIDES="winemenubuilder.exe=d"
@@ -32,9 +34,9 @@ export EDITOR='gvim -f'
 
 # java workarounds
 export AWT_TOOLKIT="MToolkit"
-export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true"
+# export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true"
 
-# dynamically set according to ssh_client
+# dynamically set pulse server according to ssh_client
 if [ -n "$SSH_CLIENT" ]; then
     export PULSE_SERVER=${SSH_CLIENT/ [ 0-9]*/}
 fi
