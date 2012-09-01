51bd8807ff1b3408f977caf10aea28854b1a1cab
Wu Jun <quark@laptop.dorm.lihdd.net>
2012-09-01 23:27:18 +0800

    profile.d: update PATH

--- .profile.d/envrc.sh
+++ .profile.d/envrc.sh
@@ -1,25 +1,13 @@
 # locale
-if locale -a | grep -Fq 'zh_CN.utf8'; then
+if [ -n "$LANG" ] && locale -a | grep -Fq 'zh_CN.utf8'; then
     export LANG=en_US.UTF-8
-    export LC_CTYPE="zh_CN.UTF-8" # KDE needs this
-    export LC_NUMERIC="en_US.UTF-8"
-    export LC_TIME="en_US.UTF-8"
-    export LC_COLLATE="en_US.UTF-8"
-    export LC_MONETARY="en_US.UTF-8"
-    export LC_MESSAGES="en_US.UTF-8"
-    export LC_PAPER="en_US.UTF-8"
-    export LC_NAME="en_US.UTF-8"
-    export LC_ADDRESS="en_US.UTF-8"
-    export LC_TELEPHONE="en_US.UTF-8"
-    export LC_MEASUREMENT="en_US.UTF-8"
-    export LC_IDENTIFICATION="en_US.UTF-8"
-    export LESSCHARSET=utf-8
+    export LC_CTYPE=zh_CN.UTF-8 # KDE needs this
 fi
 
 # prepend PATHs
 local PATH_PREPEND=''
-for p in /usr/lib/colorgcc/bin $HOME/batch .; do
-    if (! echo $PATH:$PATH_PREPEND | fgrep -q ":$p") && [ -e "$p" ]; then
+for p in /usr/lib/colorgcc/bin $HOME/batch $HOME/bin/scripts $HOME/bin $HOME/.rbenv/bin .; do
+    if (! echo $PATH:$PATH_PREPEND | fgrep -q "$p:") && [ -d "$p" ]; then
         PATH_PREPEND=${PATH_PREPEND}${p}:
     fi
 done
