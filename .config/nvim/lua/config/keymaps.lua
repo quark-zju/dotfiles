01a005e53630f95700bbfc25c7053d2f71131a80
Jun Wu <quark@lihdd.net>
2024-05-29 22:54:38 -0700

    nvim: remove Ctrl-L set by LazyVim in terminal mode

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- LazyVim sets Ctrl-h/j/k/l to move Window, but Ctrl-L is also useful in terminal...
+vim.api.nvim_del_keymap("t", "<C-l>")
