1a016660f44f87c52acd74bae911f2782b206052
Jun Wu <quark@lihdd.net>
2025-02-15 15:58:41 -0800

    nvim: update disabled plugins

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,25 +1,17 @@
 return {
-  { "markdown-preview.nvim", enabled = false },
-  { "folke/flash.nvim", enabled = false }, -- break "/" search expectation
-  -- disable (annoying) mini.indentscope animation
-  {
-    "echasnovski/mini.indentscope",
-    enabled = false,
-  },
-  -- disable (annoying) auto pairing {}, "", ''.
-  {
-    "echasnovski/mini.pairs",
-    enabled = false,
-  },
-  -- plugins that seem over fancy...
-  { "echasnovski/mini.ai", enabled = false },
-  { "echasnovski/mini.comment", enabled = false },
-  { "echasnovski/mini.ai", enabled = false },
-  { "folke/noice.nvim", enabled = false },
-  { "nvimdev/dashboard-nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "folke/persistence.nvim", enabled = false },
-  { "stevearc/dressing.nvim", enabled = false },
+  { "markdown-preview.nvim",        enabled = false },
+  -- plugins that are too fancy thus annoying...
+  { "echasnovski/mini.ai",          enabled = false },
+  { "echasnovski/mini.animate",     enabled = false },
+  { "echasnovski/mini.comment",     enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "echasnovski/mini.pairs",       enabled = false },
+  --  { "folke/flash.nvim",             enabled = false }, -- break "/" search expectation
+  --  { "folke/noice.nvim",             enabled = false },
+  { "nvimdev/dashboard-nvim",       enabled = false },
+  { "rcarriga/nvim-notify",         enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim",  enabled = false },
+  { "folke/persistence.nvim",       enabled = false },
+  { "stevearc/dressing.nvim",       enabled = false },
   { "rafamadriz/friendly-snippets", enabled = false },
 }
