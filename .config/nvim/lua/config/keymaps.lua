787e48ffb6104f3e05cb5f5523ea069c78c71a7b
Jun Wu <quark@lihdd.net>
2025-06-03 16:39:47 -0700

    nvim: add Ctrl+J/K/G keymaps
    Move vertically and grep words.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,40 @@
 
 -- LazyVim sets Ctrl-h/j/k/l to move Window, but Ctrl-L is also useful in terminal...
 -- vim.api.nvim_del_keymap("t", "<C-l>")
+
+-- Ctrl+G: Search current word or selection
+vim.keymap.set({ "n", "v" }, "<C-g>", function()
+  local selection = vim.fn.visualmode() == "V" and vim.fn.getreg('"') or vim.fn.expand("<cword>")
+  require("fzf-lua").live_grep({ query = selection })
+end)
+
+-- Ctrl+K: Move up vertically to the first non-space character.
+vim.keymap.set({ "n", "v" }, "<C-k>", function()
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local current_row = cursor[1]
+  local current_col = cursor[2]
+  for row = current_row - 1, 1, -1 do
+    local line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1] or ""
+    local char_at_col = line:sub(current_col + 1, current_col + 1)
+    if char_at_col ~= "" and char_at_col ~= " " and char_at_col ~= "\t" then
+      vim.api.nvim_win_set_cursor(0, { row, current_col })
+      return
+    end
+  end
+end)
+
+-- Ctrl+J: Move down vertically to the first non-space character.
+vim.keymap.set({ "n", "v" }, "<C-j>", function()
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local current_row = cursor[1]
+  local current_col = cursor[2]
+  local total_lines = vim.api.nvim_buf_line_count(0)
+  for row = current_row + 1, total_lines do
+    local line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1] or ""
+    local char_at_col = line:sub(current_col + 1, current_col + 1)
+    if char_at_col ~= "" and char_at_col ~= " " and char_at_col ~= "\t" then
+      vim.api.nvim_win_set_cursor(0, { row, current_col })
+      return
+    end
+  end
+end)
