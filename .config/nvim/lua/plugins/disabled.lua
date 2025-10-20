ad5fdeaf472e3b2c8524b91f5dbfe03004591d6d
Jun Wu <quark@lihdd.net>
2025-10-20 14:00:28 -0700

    nvim: rename plugins

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,17 +1,17 @@
 return {
-  { "markdown-preview.nvim",        enabled = false },
+  { "markdown-preview.nvim", enabled = false },
   -- plugins that are too fancy thus annoying...
-  { "echasnovski/mini.ai",          enabled = false },
-  { "echasnovski/mini.animate",     enabled = false },
-  { "echasnovski/mini.comment",     enabled = false },
-  { "echasnovski/mini.indentscope", enabled = false },
-  { "echasnovski/mini.pairs",       enabled = false },
+  { "nvim-mini/mini.ai", enabled = false },
+  { "nvim-mini/mini.animate", enabled = false },
+  { "nvim-mini/mini.comment", enabled = false },
+  { "nvim-mini/mini.indentscope", enabled = false },
+  { "nvim-mini/mini.pairs", enabled = false },
   --  { "folke/flash.nvim",             enabled = false }, -- break "/" search expectation
   --  { "folke/noice.nvim",             enabled = false },
-  { "nvimdev/dashboard-nvim",       enabled = false },
-  { "rcarriga/nvim-notify",         enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim",  enabled = false },
-  { "folke/persistence.nvim",       enabled = false },
-  { "stevearc/dressing.nvim",       enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+  { "stevearc/dressing.nvim", enabled = false },
   { "rafamadriz/friendly-snippets", enabled = false },
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -8,7 +8,7 @@ return {
     },
   },
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       opts.PATH = "append"
       return opts
